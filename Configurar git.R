0ee2b877eed3688716f05fdb460244e49b9a05cc
Armando Nahmias <armando@informartizar.com>
2023-11-20 23:57:59 -0400

    Alterada configuração para global em vez de apenas para o projeto corrente.

--- Configurar git.R
+++ Configurar git.R
@@ -1,15 +1,23 @@
 
-# configurando nome e email do GitHub
-usethis::use_git_config(
-  # Seu nome
-  user.name = "Armando Nahmias", 
-  # Seu email
-  user.email = "armando@informartizar.com"
-)
+# # configurando nome e email do GitHub
+# usethis::use_git_config(
+#   # Seu nome
+#   user.name = "Armando Nahmias", 
+#   # Seu email
+#   user.email = armando@informartizar.com
+# )
+
+
+# Configure Git globally
+system("git config --global user.name 'Armando Nahmias'")
+system("git config --global user.email 'armando@informartizar.com'")
+
 
 
 # Configure o Personal Access Token
 gitcreds::gitcreds_set()
+
+
 ## Enter password or token
 ## Restart R for changes to take effect
 
